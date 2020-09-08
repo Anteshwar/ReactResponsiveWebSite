8f401a43cb419b70565f189a23f55e2e26747675
Anteshwar <tondareanteshwar@gmail.com>
2020-09-08 15:51:54 +0530

    Updates

---
+++ precache-manifest.bb5112c8cb2ba9b8d4d0d0f1a6bf009f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c59261c7ff1755d41582727b61c4cec",
+    "url": "/ReactResponsiveWebSite/index.html"
+  },
+  {
+    "revision": "abfccd457ee8dc0cb171",
+    "url": "/ReactResponsiveWebSite/static/css/main.5590c9aa.chunk.css"
+  },
+  {
+    "revision": "e0629cd63e2db2d1a172",
+    "url": "/ReactResponsiveWebSite/static/js/2.ec3a467e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactResponsiveWebSite/static/js/2.ec3a467e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abfccd457ee8dc0cb171",
+    "url": "/ReactResponsiveWebSite/static/js/main.5f56cc58.chunk.js"
+  },
+  {
+    "revision": "2ce0e89d8f5a90ba8863",
+    "url": "/ReactResponsiveWebSite/static/js/runtime-main.584ece64.js"
+  }
+]);
